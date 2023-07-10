d24330219d95ebdaa6f13c35e62f1bf10a1afa3d
Monica Andres <37987281+mols2ma@users.noreply.github.com>
2023-07-10 14:10:50 -0400

    feat: set up minimal docker environment
    set docker environment so that container instance and basic, non-functional python app can be successfully spun up and ran using docker-compose.yml

---
+++ app/src/app.py
@@ -0,0 +1,8 @@
+from utils import logging
+from ingestion import Ingestor
+
+if __name__ == "__main__":
+
+    logging.info("Transcoder pipeline started...")
+
+    ingestor = Ingestor()
---
+++ app/src/ingestion.py
@@ -0,0 +1,4 @@
+from utils import logging
+class Ingestor:
+    def __init__(self):
+
