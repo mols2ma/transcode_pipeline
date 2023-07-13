e1ca5e1b75614aa5a6d6bc5786efb605a267dcc6
Monica Andres <37987281+mols2ma@users.noreply.github.com>
2023-07-13 01:09:58 -0400

    refactor: utils.py
    removed unused imports, grouped related util functions with docstrings

--- app/src/utils.py
+++ app/src/utils.py
@@ -1,19 +1,36 @@
-import time
+import os
 import logging
 import mimetypes
 import ffmpeg
-from watchdog.events import FileSystemEventHandler
-from watchdog.observers import Observer
-from transcoder import Transcoder
-from data_processor import DataProcessor
-import traceback
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import create_engine
 
-# TODO MSA: create util class objects for each component of pipeline
-
+"""Global logging"""
 logger = logging
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
+
+"""Utility functions relating to database"""
+Base = declarative_base()
+
+
+def create_database_engine():
+    db = os.environ.get('POSTGRES_DB')
+    user = os.environ.get('POSTGRES_USER')
+    password = os.environ.get('POSTGRES_PASSWORD')
+    return create_engine(f'postgresql://{user}:{password}@db:5432/{db}', echo=True)
+
+
+def init_db():
+    db_engine = create_database_engine()
+    db_engine.echo = False
+    Base.metadata.create_all(db_engine)
+    return sessionmaker(bind=db_engine)
+
+
+"""Utility functions relating to ffmpeg"""
+
 
 def get_file_metadata(file_path):
     try:
@@ -21,16 +38,23 @@ def get_file_metadata(file_path):
     except:
         raise Exception(f"Failed to analyze file at {file_path}")
 
+
+"""Utility functions relating to validation"""
+
+
 def validate_video(file_path):
     try:
         if "video" in mimetypes.guess_type(file_path)[0]:
             fm = get_file_metadata(file_path)
             if fm is None or fm['codec_type'] != "video":
-                raise Exception(f"File at {file_path} is not of type is not of video.")
+                raise Exception(
+                    f"File at {file_path} is not of type is not of video.")
         else:
-            raise Exception(f"File at {file_path} is not of type is not of video.")
+            raise Exception(
+                f"File at {file_path} is not of type is not of video.")
     except Exception as e:
         raise Exception(e)
+
 
 """
 If yes then add function or logic to check if its a valid imdbid file name
@@ -39,16 +63,7 @@ If yes then add function or logic to check if its a valid imdbid file name
             https://developer.imdb.com/documentation/key-concepts
             https://stackoverflow.com/questions/57746487/how-to-run-the-regexmatchingeventhandler-of-watchdog-correctly)
 """
+
+
 def is_tmdb_id():
     return
-
-def trigger_pipeline(file_path, session_db):
-    try:
-        validate_video(file_path)
-        logging.info(f"Detected and validated video file at {file_path}")
-
-        transcoder = Transcoder(file_path) # TODO MSA: refactor to consider continuation of class or method
-
-        result = DataProcessor(file_path, session_db) # TODO MSA: refactor to consider continuation of class or method
-    except Exception as e:
-        logging.info(f"Pipeline error: {e}\n{traceback.format_exc()}")
